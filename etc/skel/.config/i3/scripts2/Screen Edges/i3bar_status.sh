362cd6e5a5eb79e94917b173a9035230019a3f82
Mahmoud SH <mysh264@gmail.com>
2026-05-11 18:43:59 +0400

    Update i3bar_status.sh

--- etc/skel/.config/i3/scripts2/Screen Edges/i3bar_status.sh
+++ etc/skel/.config/i3/scripts2/Screen Edges/i3bar_status.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-i3-msg -t get_bar_config bar-0 | while read -r line; do
-    MODE=$(echo "$line" | grep -o dock)
+# Fetch the bar configuration for bar-0
+# We use jq to specifically look at the "mode" property
+MODE=$(i3-msg -t get_bar_config bar-0 | jq -r '.mode')
 
-    if [[ "$MODE" == "dock" ]]; then
-    echo "Nothing to do"
-    else
+if [[ "$MODE" == "dock" ]]; then
+    # If the bar is already in dock mode, do nothing to prevent flickering
+    exit 0
+else
+    # If the bar is in 'hide' or 'invisible' mode, toggle the hidden_state
     i3-msg bar hidden_state toggle
-    fi
-done
+fi
