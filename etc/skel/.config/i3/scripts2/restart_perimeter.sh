1ca2df6f39d02434f0c5e6d751dea4171f3036b6
Mahmoud SH <mysh264@gmail.com>
2026-06-02 04:09:55 +0400

    Update restart_perimeter.sh

--- etc/skel/.config/i3/scripts2/restart_perimeter.sh
+++ etc/skel/.config/i3/scripts2/restart_perimeter.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -x
+
 # --- CONFIGURATION ---
 SCRIPTS=(
     "$HOME/.config/i3/scripts2/Screen Edges/master_perimeter_control.sh"
     "$HOME/.config/i3/scripts2/KDE Plasma/plasma-i3-orchestrator.sh"
     "$HOME/.config/i3/scripts2/Workspace/track_workspaces.sh"
-    # "/path/to/your/fifth_script.sh"
+    "$HOME/.config/i3/scripts2/fehbg.sh"
     # "/path/to/your/sixth_script.sh"
 )
 
 echo "--- Perimeter Manager: Restarting Stack ---"
+
+sleep 5
 
 for SCRIPT_PATH in "${SCRIPTS[@]}"; do
     if [[ -f "$SCRIPT_PATH" ]]; then
@@ -18,14 +22,17 @@ for SCRIPT_PATH in "${SCRIPTS[@]}"; do
         # 1. Kill the old process using a precise pattern
         # We use a regex to ensure we don't kill this manager script itself
         echo "Stopping: $SCRIPT_NAME"
-        pkill -f "/$SCRIPT_NAME"
+        #pkill -f "/$SCRIPT_NAME"
+        pkill -f "bash $SCRIPT_PATH"
         sleep 0.2
 
         # 2. Launch in the background and DISOWN
         # The '&' makes it a background task
         # 'disown' lets the manager exit without killing the child
         echo "Starting: $SCRIPT_NAME"
-        bash "$SCRIPT_PATH" > /dev/null 2>&1 &
+        #bash "$SCRIPT_PATH" > /dev/null 2>&1 &
+        bash "$SCRIPT_PATH" &>> $HOME/.scripts_log/"$SCRIPT_NAME".log &
+
         disown
     else
         echo "Warning: Script not found at $SCRIPT_PATH"
